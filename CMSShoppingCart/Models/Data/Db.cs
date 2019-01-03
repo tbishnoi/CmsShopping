2ce9a00e67ac692a3dd5b0cb953f968133b4fac7
tushar bishnoi <tushar.bishnoi@gmail.com>
2019-01-03 12:56:50 +0530

    Added DTO and View Model

---
+++ CMSShoppingCart/Models/Data/Db.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CMSShoppingCart.Models.Data
+{
+    public class Db : DbContext
+    {
+
+    }
+}
---
+++ CMSShoppingCart/Models/ViewModels/PagesVM.cs
@@ -0,0 +1,35 @@
+﻿using CMSShoppingCart.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CMSShoppingCart.Models.ViewModels
+{
+    public class PagesVM
+    {
+
+        public PagesVM(PagesDTO row)
+        {
+            this.Id = row.Id;
+            this.Title = row.Title;
+            this.Slug = row.Slug;
+            this.Body = row.Body;
+            this.Sorting = row.Sorting;
+            this.HasSideBar = row.HasSideBar;
+        }
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Slug { get; set; }
+
+        public string Body { get; set; }
+
+        public int Sorting { get; set; }
+
+        public bool HasSideBar { get; set; }
+
+    }
+}
